2b4a00c04fc84bcff4b8c90fe2113049068e72d1
apklock <apklock@gmail.com>
2015-03-03 20:01:41 -0800

    added html models, forms, need to fix registration

--- myblog/views.py
+++ myblog/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, loader
 from myblog.models import Post
+from django import forms
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'registration_form.html', {'form': form})
 
 
 def stub_view(request, *args, **kwargs):
